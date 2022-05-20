ab48c07d644786d85575472a022603273bc333f4
HeatherH <hherries@yahoo.com>
2022-05-20 14:43:44 -0600

    Template Literals

--- app.js
+++ app.js
@@ -1,43 +1,29 @@
-// Strings Methods and Concatenation
-
-const firstName = "Heather";
-const lastName = "Herries";
+// Template Literals
+const name = "Heather";
 const age = 46;
-const string = "Hi My name is Heather";
+const job = "Web Developer";
+const city = "Denver";
+let html;
 
-let val;
-val = firstName + lastName;
+// Without Template Strings (ES5)
+html =
+  "<ul><li>Name: " +
+  name +
+  "</li><li>Age: " +
+  age +
+  " </li><li>Job: " +
+  job +
+  " </li><li>City: " +
+  city +
+  " </li></ul>";
 
-// Concatenation
-val = firstName + " " + lastName;
-val = "Hello my name is " + firstName + " and I am " + age;
-
-// Appending
-val = "Kristen ";
-val += "Smith";
-
-// length
-val =
-  firstName.length; /* this will count the amount of characters in the string */
-
-// concat()
-val = firstName.concat(" ", lastName);
-
-// Change case
-val = firstName.toUpperCase();
-val = firstName.toLowerCase();
-
-// slice()
-val = firstName.slice(0, 4);
-
-// split()
-val = string.split(
-  " "
-); /* this will turn a string into an array split at the spaces */
-
-// replace()
-val = string.replace("Heather", "Mariam");
-
-// includes()
-val = string.includes("Hi");
-console.log(val);
+// With Template Strings (ES6)
+html = `
+<ul>
+  <li>Name: ${name}</li>
+  <li>Age: ${age}</li>
+  <li>Job: ${job}</li>
+  <li>City: ${city}</li>  
+</ul>
+`;
+document.body.innerHTML = html;
