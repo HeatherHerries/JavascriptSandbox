d31c01d279f1f27051c4fc5354923cdee9b1470a
HeatherH <hherries@yahoo.com>
2022-05-20 16:51:31 -0600

    Arrays and Array Methods

--- app.js
+++ app.js
@@ -1,29 +1,56 @@
-// Template Literals
-const name = "Heather";
-const age = 46;
-const job = "Web Developer";
-const city = "Denver";
-let html;
+// Arrays and Array Methods
+const number = [1, 2, 3, 4, 5];
 
-// Without Template Strings (ES5)
-html =
-  "<ul><li>Name: " +
-  name +
-  "</li><li>Age: " +
-  age +
-  " </li><li>Job: " +
-  job +
-  " </li><li>City: " +
-  city +
-  " </li></ul>";
+//Array created with an array constructo
+const cars = new Array("Buick", "Chevrolet", "Fiat", "Mazda");
 
-// With Template Strings (ES6)
-html = `
-<ul>
-  <li>Name: ${name}</li>
-  <li>Age: ${age}</li>
-  <li>Job: ${job}</li>
-  <li>City: ${city}</li>  
-</ul>
-`;
-document.body.innerHTML = html;
+const fruit = ["Apple", "Banana", "Orange", "Pear"];
+
+// Arrays can be created with a mixture of data types
+
+const mixed = [22, "Hello", true, undefined, null, { a: 1, b: 2 }, new Date()];
+
+let val;
+
+// Get array length or how many items are in the array
+val = number.length;
+
+// Check if something is an array
+val = Array.isArray(number);
+
+// Get a single value
+val = number[3];
+
+// Insert into an array
+number[2] = 100;
+
+// Find index of a value
+val = number.indexOf(100);
+
+// MUTATING ARRAYS
+
+// Add to the end of an array
+number.push(250);
+
+// Add to the beginning
+number.unshift(45);
+
+// Remove from end
+number.pop();
+
+// Remove from front
+number.shift();
+
+// Remove from values from within
+number.splice(1, 3);
+
+// Reverse the array
+number.reverse();
+
+// Concatenate Arrays
+val = number.concat(cars);
+
+// Sorting Arrays
+val = fruit.sort();
+console.log(number);
+console.log(val);
