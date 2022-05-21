ed8955d72c1d5ccf15236e0e311385b67b9557b8
HeatherH <hherries@yahoo.com>
2022-05-21 11:08:31 -0600

    Dates and Times

--- app.js
+++ app.js
@@ -1,35 +1,30 @@
-// Object Literals
-const person = {
-  firstName: "Heather",
-  lastName: "Herries",
-  age: 46,
-  hobbies: ["bowling", "art", "diy"],
-  address: {
-    city: "Denver",
-    state: "CO"
-  },
-  getBirthYear: function() {
-    return 2022 - this.age;
-  }
-};
+// Dates & Times
+const today = new Date();
+let birthday = new Date("9-30-1975");
+birthday = new Date("July 15 1983");
+birthday = new Date("6/23/1954");
 
-let val = person;
-val = person.firstName;
-val = person.lastName;
-val = person.age;
-val = person.hobbies;
-val = person.hobbies[1];
-val = person.address;
-val = person.address.state;
-val = person.getBirthYear();
+let val;
+
+val = today.getMonth();
+val = today.getDate();
+val = today.getDay();
+val = today.getFullYear();
+val = today.getHours();
+val = today.getMinutes();
+val = today.getSeconds();
+val = today.getMilliseconds();
+val = today.getTime();
 
 console.log(val);
 
-const people = [
-  { name: "John", age: 30 },
-  { name: "Sara", age: 25 }
-];
+// Date Manipulation
 
-for (let i = 0; i < people.length; i++) {
-  console.log(people[i].age);
-}
+birthday.setMonth(2);
+birthday.setDate(12);
+birthday.setFullYear(1920);
+birthday.setHours(3);
+birthday.setMinutes(20);
+birthday.setSeconds(22);
+
+console.log(birthday);
