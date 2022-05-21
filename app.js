830f2c82e357356e05bb84a6b9a88c81c5d90e52
HeatherH <hherries@yahoo.com>
2022-05-21 12:29:21 -0600

    Switches

--- app.js
+++ app.js
@@ -1,95 +1,42 @@
-// If Statements and Comparison Operators
-
-// General Syntax
-// if(something) {
-//   do something
-// } else (
-//   do something else
-// )
-
-const id = 100;
-
-// Equal To
-if (id == 100) {
-  console.log("CORRECT");
-} else {
-  console.log("INCORRECT");
-}
-
-// Not Equal To
-if (id != 100) {
-  console.log("CORRECT");
-} else {
-  console.log("INCORRECT");
-}
-
-// Equal To Value & Type
-if (id === 100) {
-  console.log("CORRECT");
-} else {
-  console.log("INCORRECT");
-}
-
-// Not Equal To Value & Type
-if (id !== 100) {
-  console.log("CORRECT");
-} else {
-  console.log("INCORRECT");
-}
-
-// Test if undefined
-let bird;
-
-if (typeof bird !== "undefined") {
-  console.log(`The Bird is a ${bird}`);
-} else {
-  console.log(`There is no bird`);
-}
-
-// Greater than or less than
-if (id > 200) {
-  console.log("CORRECT");
-} else {
-  console.log("INCORRECT");
-}
-
-// else if
-
+// Switches
 const color = "yellow";
 
-if (color === "red") {
-  console.log("The color is red");
-} else if (color === "blue") {
-  console.log("The color is blue");
-} else {
-  console.log("The color is neither red nor blue");
+switch (color) {
+  case "red":
+    console.log("The color is red");
+    break;
+  case "blue":
+    console.log("The color is blue");
+    break;
+  default:
+    console.log("Color is not red or blue");
+    break;
 }
 
-// Logical Operators
-const name = "Heather";
-const age = 20;
+let day;
 
-// And &&
-if (age > 0 && age < 12) {
-  console.log(` ${name} is a child`);
-} else if (age >= 13 && age <= 19) {
-  console.log(`${name} is a teenager`);
-} else {
-  console.log(`${name} is an adult`);
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+    break;
 }
-// Or ||
-if (age < 16 || age > 65) {
-  console.log(`${name} can't enter the race`);
-} else {
-  console.log(`${name} can enter the race`);
-}
 
-// Ternary Operator (Shorthand for an if / else statement)
-// General Syntax
-// (something === something ? do something : do something else)
-
-const syblingType = "brother";
-
-console.log(
-  syblingType === "sister" ? "This is a sister" : "This is a brother"
-);
+console.log(`Today is ${day}`);
