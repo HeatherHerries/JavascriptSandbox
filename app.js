8f930fc4461b7310969ecbd85ee5e8da294b01f5
HeatherH <hherries@yahoo.com>
2022-05-21 12:15:20 -0600

    If Statements and Comparison Operators

--- app.js
+++ app.js
@@ -1,30 +1,95 @@
-// Dates & Times
-const today = new Date();
-let birthday = new Date("9-30-1975");
-birthday = new Date("July 15 1983");
-birthday = new Date("6/23/1954");
+// If Statements and Comparison Operators
 
-let val;
+// General Syntax
+// if(something) {
+//   do something
+// } else (
+//   do something else
+// )
 
-val = today.getMonth();
-val = today.getDate();
-val = today.getDay();
-val = today.getFullYear();
-val = today.getHours();
-val = today.getMinutes();
-val = today.getSeconds();
-val = today.getMilliseconds();
-val = today.getTime();
+const id = 100;
 
-console.log(val);
+// Equal To
+if (id == 100) {
+  console.log("CORRECT");
+} else {
+  console.log("INCORRECT");
+}
 
-// Date Manipulation
+// Not Equal To
+if (id != 100) {
+  console.log("CORRECT");
+} else {
+  console.log("INCORRECT");
+}
 
-birthday.setMonth(2);
-birthday.setDate(12);
-birthday.setFullYear(1920);
-birthday.setHours(3);
-birthday.setMinutes(20);
-birthday.setSeconds(22);
+// Equal To Value & Type
+if (id === 100) {
+  console.log("CORRECT");
+} else {
+  console.log("INCORRECT");
+}
 
-console.log(birthday);
+// Not Equal To Value & Type
+if (id !== 100) {
+  console.log("CORRECT");
+} else {
+  console.log("INCORRECT");
+}
+
+// Test if undefined
+let bird;
+
+if (typeof bird !== "undefined") {
+  console.log(`The Bird is a ${bird}`);
+} else {
+  console.log(`There is no bird`);
+}
+
+// Greater than or less than
+if (id > 200) {
+  console.log("CORRECT");
+} else {
+  console.log("INCORRECT");
+}
+
+// else if
+
+const color = "yellow";
+
+if (color === "red") {
+  console.log("The color is red");
+} else if (color === "blue") {
+  console.log("The color is blue");
+} else {
+  console.log("The color is neither red nor blue");
+}
+
+// Logical Operators
+const name = "Heather";
+const age = 20;
+
+// And &&
+if (age > 0 && age < 12) {
+  console.log(` ${name} is a child`);
+} else if (age >= 13 && age <= 19) {
+  console.log(`${name} is a teenager`);
+} else {
+  console.log(`${name} is an adult`);
+}
+// Or ||
+if (age < 16 || age > 65) {
+  console.log(`${name} can't enter the race`);
+} else {
+  console.log(`${name} can enter the race`);
+}
+
+// Ternary Operator (Shorthand for an if / else statement)
+// General Syntax
+// (something === something ? do something : do something else)
+
+const syblingType = "brother";
+
+console.log(
+  syblingType === "sister" ? "This is a sister" : "This is a brother"
+);
