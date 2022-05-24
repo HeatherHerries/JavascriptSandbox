f85d83288878d8e4640e922585c1026c36ce937b
HeatherH <hherries@yahoo.com>
2022-05-23 19:36:43 -0600

    DOM Selectorsfor Single Elements

--- app.js
+++ app.js
@@ -1,39 +1,30 @@
-// Examining The Document Object
-let val;
+// DOM Selectors for Single Elements
 
-val = document;
-val = document.all;
-val = document.all[2];
-val = document.head;
-val = document.body;
-val = document.doctype;
-val = document.domain;
-val = document.URL;
-val = document.characterSet;
-val = document.contentType;
+// document.getElementById()
 
-val = document.forms;
-val = document.forms[0];
-val = document.forms[0].id;
-val = document.forms[0].method;
-val = document.forms[0].action;
+console.log(document.getElementById("task-title"));
 
-val = document.links;
-val = document.links[0];
-val = document.links[0].id;
-val = document.links[0].className;
-val = document.links[0].classList[0];
+// Get things from the element
+console.log(document.getElementById("task-title").id);
+console.log(document.getElementById("task-title").className);
 
-val = document.images;
+const taskTitle = document.getElementById("task-title");
 
-val = document.scripts;
-val = document.scripts[2].getAttribute("src");
+// Change Styling
+taskTitle.style.background = "#333";
+taskTitle.style.color = "#fff";
+taskTitle.style.padding = "5px";
 
-let scripts = document.scripts;
+// Change Content
+taskTitle.textContent = "Task List";
+taskTitle.innerText = "My Task List";
+taskTitle.innerHTML = '<span style="color:red">Task List</span>';
 
-let scriptArr = Array.from(scripts);
-
-scriptsArr.forEach(function(script) {
-  console.log(script.getAttribute("src"));
-});
-console.log(val);
+// document.querySelector
+console.log(document.querySelector("#task-title"));
+console.log(document.querySelector(".card-title"));
+console.log(document.querySelector("h5"));
+document.querySelector("li").style.color = "red";
+document.querySelector("li:last-child").style.color = "red";
+document.querySelector("li:nth-child(3)").style.color = "blue";
+document.querySelector("li:nth-child(4)").textContent = "Favorite Task";
