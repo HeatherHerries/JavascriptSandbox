6618f056f9c6bcf70c2f66926bd9e82d709e6968
HeatherH <hherries@yahoo.com>
2022-06-02 11:58:07 -0600

    Constructors and the .this Keyword

--- app.js
+++ app.js
@@ -1,43 +1,20 @@
-// Local and Session Storage
+// Constructors and the 'this' Keyword
+// Person Constructor
+function Person(name, dob) {
+  this.name = name;
+  // this.age = age;
+  this.birthday = new Date(dob);
+  this.calcAge = function() {
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  };
+}
 
-// Set Local Storage Item
-localStorage.setItem("name", "John");
-localStorage.setItem("age", "30");
+// const heather = new Person("Heather", dob);
+// const john = new Person("John", 20);
 
-// Set Session Storage Item
-sessionStorage.setItem("name", "Beth");
+// console.log(john.age, john.name);
 
-// Remove from Storage
-localStorage.removeItem("name");
-
-// Get from Storage
-const name = localStorage.getItem("name");
-const age = localStorage.getItem("age");
-
-// Clear Local Storage
-localStorage.clear();
-console.log(name, age);
-
-document.querySelector("form").addEventListener("submit", function(e) {
-  const task = document.getElementById("task").value;
-
-  let task;
-
-  if (localStorage.getItem("tasks") === null) {
-    tasks = [];
-  } else {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-  }
-
-  tasks.push(task);
-
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-  alert("Task Saved");
-  e.preventDefault();
-});
-
-const tasks = JSON.parse(localStorage.getItem("tasks"));
-
-tasks.forEach(function(task) {
-  console.log(task);
-});
+const heather = new Person("Heather", "9-30-1975");
+console.log(heather.calcAge());
