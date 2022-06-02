6d456a4831e8529dfa247fefb22260c517458c5f
HeatherH <hherries@yahoo.com>
2022-06-02 12:23:45 -0600

    Build Constructors

--- app.js
+++ app.js
@@ -1,20 +1,45 @@
-// Constructors and the 'this' Keyword
-// Person Constructor
-function Person(name, dob) {
-  this.name = name;
-  // this.age = age;
-  this.birthday = new Date(dob);
-  this.calcAge = function() {
-    const diff = Date.now() - this.birthday.getTime();
-    const ageDate = new Date(diff);
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
-  };
+// Build Constructors
+// String
+const name1 = "Jeff";
+const name2 = new String("George");
+
+// console.log(name2);
+
+// console.log(typeof name2);
+
+if (name2 === "Jeff") {
+  console.log("YES");
+} else {
+  console.log("NO");
 }
 
-// const heather = new Person("Heather", dob);
-// const john = new Person("John", 20);
+// Number
+const num1 = 5;
+const num2 = new Number(5);
 
-// console.log(john.age, john.name);
+console.log(num2);
 
-const heather = new Person("Heather", "9-30-1975");
-console.log(heather.calcAge());
+// Boolean
+const boo1 = true;
+const boo2 = new Boolean(true);
+
+// Function
+const getSum1 = function(x, y) {
+  return x + y;
+};
+
+const getSum2 = new Function("x", "y", "return 1 + 1");
+
+console.log(getSum1(1, 1));
+
+// Object
+const john1 = { name: "John" };
+const john2 = new Object({ name: "John" });
+
+// Array
+const arr1 = [1, 2, 3, 4];
+const arr2 = new Array(1, 2, 3, 4);
+
+// Regular Expressions
+const re1 = /\w+/;
+const re2 = new RegExp("\\w+");
