4f082706c2bc68991c8f3a15ab20e08d60eb0570
HeatherH <hherries@yahoo.com>
2022-07-05 17:15:38 -0600

    Regular Expressions: EvaluationFunctions

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
-const user = { email: "jdoe@gmail.com" };
+let re;
 
-try {
-  // Produce a ReferenceError
-  // myFunction();
-  // Produce a TypeError
-  // null.myFunction();
+re = /hello/;
+// i = case insensitive
+re = /hello/i;
+// g = Global Search
+// re = /hello/g;
 
-  if (!user.name) {
-    // throw "User has no name";
-    throw new SyntaxError("User has no name");
-  }
-} catch (e) {
-  console.log(`User Error: ${e.message}`);
-  // console.log(e);
-  // console.log(e.message);
-  // console.log(e.name);
-  console.log(e instanceof ReferenceError);
-} finally {
-  console.log("Finally runs regardless of result...");
-}
+// console.log(re);
+// console.log(re.source);
 
-console.log("Program continues...");
+// exec() - Return result in an array or null
+// const result = re.exec("hello world");
+// console.log(result);
+// console.log(result[0]);
+// console.log(result.index);
+// console.log(result.input);
+
+// test() - returns true of false
+// const result = re.test("Hello");
+// console.log(result);
+
+// match() - Return result array or null
+// const str = "Hello There";
+// const result = str.match(re);
+// console.log(result);
+
+// search() - Returns index of the first match if not found returns -I
+// const str = "Hello There";
+// const result = str.search(re);
+// console.log(result);
+
+// replace() - Return new string with some or all matches of a patter
+const str = "Hello There";
+const newStr = str.replace(re, "Hi");
+console.log(newStr);
